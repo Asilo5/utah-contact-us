e5dd78f101d7cb2fa46f709c8e57df03ee7c2791
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-19 14:00:43 -0700

    Add Import of files in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import Name from './Components/Name';
+import Email from './Components/Email';
+import BirthDate from './Components/BirthDate';
+import Button from './Components/Button';
+
 const App = () => {
 
   return (
     <section>
         <h1>Contact Form</h1>
         <form>
-           
+           <Name />
+           <Email />
+           <BirthDate />
+           <Button />
         </form>
+        {/* add aggrement to be contacted must be checked to make button work */}
     </section>
   )
 }
