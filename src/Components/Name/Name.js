d57a487c8826b16ef8ad6d81b15463bd0b9e1a1f
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-19 13:29:59 -0700

    Add basic layout of Name component

---
+++ src/Components/Name/Name.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Name.css';
+
+const Name = () => {
+  return (
+      <section>
+          <label>Name</label>
+          <input type='text'/>
+      </section>
+  )
+}
+
+export default Name;
