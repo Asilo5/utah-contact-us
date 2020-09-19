5a963ff4694db6f25b3774aa2b981352e5ef8432
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-19 13:34:15 -0700

    Add basic layout to Birthdate component

---
+++ src/Components/BirthDate/BirthDate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BirthDate.css';
+
+const BirthDate = () => {
+  return (
+      <section>
+          <label>BirthDate</label>
+          <input type='date'/>
+      </section>
+  )
+}
+
+export default BirthDate;
