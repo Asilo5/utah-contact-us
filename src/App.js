d971fc2afc75490a3fce360eec724ba2a49d77e3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-21 13:34:18 -0700

    Add functionality of inputs to see if it updates states

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 
 import { sendUserInfo } from './apiCalls';
 
-
 const App = () => {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [birthdate, setBirthdate] = React.useState('');
+  const [subscribe, setSubscribe] = React.useState(false);
+
+  let handleClick = () => {
+    setSubscribe(!subscribe);
+  }
 
   return (
     <section className='contact_form'>
@@ -12,19 +19,34 @@ const App = () => {
         <form>
           <section>
             <label>Name</label>
-            <input type='text'/>
+            <input type='text'
+                   name='name'
+                   value={name}
+                   onChange={(e) => setName(e.target.value)}
+            />
           </section>
           <section>
             <label>Email</label>
-            <input type='email'/>
+            <input type='email'
+                   name='email'
+                   value={email}
+                   onChange={(e) => setEmail(e.target.value)}
+            />
           </section>
           <section>
             <label>BirthDate</label>
-            <input type='date'/>
+            <input type='date'
+                   name='birthdate'
+                   value={birthdate}
+                   onChange={(e) => setBirthdate(e.target.value)}
+            />
           </section>
           <section>
             <label>Would you like to subscribe your email?</label>
-            <input type='radio'/>
+            <input type='checkbox'
+                   checked={subscribe}
+                   onClick={handleClick}
+            />
           </section>
         </form>
         <button>Submit</button>
